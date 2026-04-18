fd3aa07abe03e719eb715acdf1bf71251db842aa
danlerr <danieleantonucci02@gmail.com>
2026-04-18 12:37:33 +0200

    Rimossi commenti :/

--- src/main/java/alchgame/controller/ExperimentHandler.java
+++ src/main/java/alchgame/controller/ExperimentHandler.java
@@ -20,27 +20,20 @@ public class ExperimentHandler {
         this.alchemicAlgorithm = alchemicAlgorithm;
     }
 
-    // Recupera il target dall'ID fornito e verifica se richiede un pagamento in oro
-    // prima di poter condurre l'esperimento. Restituisce true se il pagamento è necessario.
     public boolean paymentCheck(String targetId) {
         this.currentTarget = gameContext.getTarget(targetId);
         return currentTarget.requiresPayment();
     }
 
-    // Restituisce la lista degli ingredienti disponibili nel laboratorio del giocatore corrente.
     public List<Ingredient> getIngredients() {
         return gameContext.getCurrentPlayer().getIngredientsFromLab();
     }
 
-    // Sottrae 1 moneta d'oro al giocatore corrente come costo dell'esperimento.
     public void payGold() {
         Player player = gameContext.getCurrentPlayer();
         player.removeGold(1);
     }
 
-    // Esegue l'esperimento: calcola la pozione risultante dai due ingredienti,
-    // registra l'esperimento nel diario del giocatore, applica l'effetto della pozione
-    // sul target corrente e restituisce la pozione ottenuta.
     public Potion conductExperiment(Ingredient i1, Ingredient i2) {
         Potion potion = alchemicAlgorithm.computePotion(i1, i2);
         gameContext.getCurrentPlayer().recordExperiment(currentTarget, i1, i2, potion);
@@ -48,8 +41,6 @@ public class ExperimentHandler {
         return potion;
     }
 
-    // Annulla la selezione del target corrente: il giocatore ha rifiutato di procedere
-    // con l'esperimento (es. non vuole pagare o ha cambiato idea).
     public void refuseExperiment() {
         this.currentTarget = null;
     }
