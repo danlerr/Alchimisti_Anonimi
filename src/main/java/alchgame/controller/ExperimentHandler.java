b60f3733762d7d8c76b8360f32d403e9bd86248e
danlerr <danieleantonucci02@gmail.com>
2026-04-18 12:34:33 +0200

    Aggiunti commenti nel ExperimentHandler

--- src/main/java/alchgame/controller/ExperimentHandler.java
+++ src/main/java/alchgame/controller/ExperimentHandler.java
@@ -1,12 +1,10 @@
 package alchgame.controller;
 
-
 import java.util.List;
 
 import alchgame.model.*;
 import alchgame.service.AlchemicAlgorithm;
 import alchgame.service.GameContext;
-
 
 /**
  * ExperimentHandler - UC08 Controller class
@@ -22,21 +20,27 @@ public class ExperimentHandler {
         this.alchemicAlgorithm = alchemicAlgorithm;
     }
 
-    /** Imposta il target e ritorna true se serve pagare oro prima di procedere. */
+    // Recupera il target dall'ID fornito e verifica se richiede un pagamento in oro
+    // prima di poter condurre l'esperimento. Restituisce true se il pagamento è necessario.
     public boolean paymentCheck(String targetId) {
         this.currentTarget = gameContext.getTarget(targetId);
         return currentTarget.requiresPayment();
     }
 
+    // Restituisce la lista degli ingredienti disponibili nel laboratorio del giocatore corrente.
     public List<Ingredient> getIngredients() {
         return gameContext.getCurrentPlayer().getIngredientsFromLab();
     }
 
+    // Sottrae 1 moneta d'oro al giocatore corrente come costo dell'esperimento.
     public void payGold() {
         Player player = gameContext.getCurrentPlayer();
         player.removeGold(1);
     }
 
+    // Esegue l'esperimento: calcola la pozione risultante dai due ingredienti,
+    // registra l'esperimento nel diario del giocatore, applica l'effetto della pozione
+    // sul target corrente e restituisce la pozione ottenuta.
     public Potion conductExperiment(Ingredient i1, Ingredient i2) {
         Potion potion = alchemicAlgorithm.computePotion(i1, i2);
         gameContext.getCurrentPlayer().recordExperiment(currentTarget, i1, i2, potion);
@@ -44,6 +48,8 @@ public class ExperimentHandler {
         return potion;
     }
 
+    // Annulla la selezione del target corrente: il giocatore ha rifiutato di procedere
+    // con l'esperimento (es. non vuole pagare o ha cambiato idea).
     public void refuseExperiment() {
         this.currentTarget = null;
     }
